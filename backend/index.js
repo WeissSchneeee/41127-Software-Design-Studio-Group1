6d50425d4e59ac6a3328636b6910ab54d03252c4
jonathanertanto <jonathanertanto@gmail.com>
2022-08-09 17:40:21 +1000

    connect backend to postgresql database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const path = require('path');
+require ("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
+
+// CONNECTION TO HEROKU POSTGRESQL DATABASE
+const {Client} = require("pg");
+const connection = new Client({
+    host: "ec2-34-207-12-160.compute-1.amazonaws.com",
+    user: "fkqtzcrjkybizb",
+    port: 5432,
+    password: "041098df80146c8615cd856429f37d05afe722c42b88f5b728f790f4c6462746",
+    database: "d5daga22vac1v1",
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+connection.connect((err) => {
+    if(err) console.log(err);
+    else console.log("Connected to database!");
+});
+exports.connection = connection;
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
